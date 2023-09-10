fe3be824e4b6f94f9f91fec74e9c322f6f89a3ef
tamura_fumiya <k021g1134@g.neec.ac.jp>
2023-09-10 15:23:31 +0900

    UI配置完了

--- Application/UIManager.h
+++ Application/UIManager.h
@@ -9,11 +9,31 @@ class UIManager
 private:
 	// スプライト
 	std::unique_ptr<Sprite> sFrame_ = nullptr;
-	std::unique_ptr<Sprite> sPlayerHpIn_ = nullptr;
+	std::unique_ptr<Sprite> sExp_ = nullptr;
+	std::unique_ptr<Sprite> sExpIn_ = nullptr;
+	std::unique_ptr<Sprite> sExpText_ = nullptr;
+	std::unique_ptr<Sprite> sHpPlayer_ = nullptr;
+	std::unique_ptr<Sprite> sHpPlayerIn_ = nullptr;
+	std::unique_ptr<Sprite> sHpTextPlayer_ = nullptr;
+	std::unique_ptr<Sprite> sHpBoss_ = nullptr;
+	std::unique_ptr<Sprite> sHpBossIn_ = nullptr;
+	std::unique_ptr<Sprite> sHpTextBoss_ = nullptr;
+	std::unique_ptr<Sprite> sKakeru2_ = nullptr;
+	std::unique_ptr<Sprite> sNextText_ = nullptr;
 
 	// 画像ハンドル
 	uint16_t hFrame_ = 0;
-	uint16_t hPlayerHpIn_ = 0;
+	uint16_t hExp_ = 0;
+	uint16_t hExpIn_ = 0;
+	uint16_t hExpText_ = 0;
+	uint16_t hHpPlayer_ = 0;
+	uint16_t hHpPlayerIn_ = 0;
+	uint16_t hHpTextPlayer_ = 0;
+	uint16_t hHpBoss_ = 0;
+	uint16_t hHpBossIn_ = 0;
+	uint16_t hHpTextBoss_ = 0;
+	uint16_t hKakeru2_ = 0;
+	uint16_t hNextText_ = 0;
 #pragma endregion
 
 #pragma region メンバ関数
