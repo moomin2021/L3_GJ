557ccd87c1f9edc29f73236de0d0dff7e1a6d935
MSI\xdrag <k021g1163@g.neec.ac.jp>
2023-09-13 09:53:01 +0900

    nextの画像サイズ修正

--- Application/PieceManager.cpp
+++ Application/PieceManager.cpp
@@ -13,24 +13,31 @@ void PieceManager::Initialize()
 	pieceIndex[Piece_I].offset = { {0,-1,},{0,0},{0,1},{0,2} };
 	pieceIndex[Piece_I].blockData = { n,n,c,c };
 	pieceIndex[Piece_I].previewTexture = LoadTexture("Resources/piece_i.png");
+	pieceIndex[Piece_I].textureSize = { 80,20 };
 	pieceIndex[Piece_L].offset = { {-1,0,},{0,0},{0,1},{0,2} };
 	pieceIndex[Piece_L].blockData = { c,c,n,n };
 	pieceIndex[Piece_L].previewTexture = LoadTexture("Resources/piece_l.png");
+	pieceIndex[Piece_L].textureSize = { 60,40 };
 	pieceIndex[Piece_J].offset = { {0,-1,},{0,0},{0,1},{1,1} };
 	pieceIndex[Piece_J].blockData = { c,c,n,n };
 	pieceIndex[Piece_J].previewTexture = LoadTexture("Resources/piece_j.png");
+	pieceIndex[Piece_J].textureSize = { 60,40 };
 	pieceIndex[Piece_O].offset = { {0,0,},{0,1},{1,0},{1,1} };
 	pieceIndex[Piece_O].blockData = { n,c,c,n };
 	pieceIndex[Piece_O].previewTexture = LoadTexture("Resources/piece_o.png");
+	pieceIndex[Piece_O].textureSize = { 40,40 };
 	pieceIndex[Piece_S].offset = { {0,-1,},{0,0},{1,0},{1,1} };
 	pieceIndex[Piece_S].blockData = { c,n,n,c };
 	pieceIndex[Piece_S].previewTexture = LoadTexture("Resources/piece_s.png");
+	pieceIndex[Piece_S].textureSize = { 60,40 };
 	pieceIndex[Piece_T].offset = { {0,-1,},{0,0},{0,1},{1,0} };
 	pieceIndex[Piece_T].blockData = { c,n,n,c };
 	pieceIndex[Piece_T].previewTexture = LoadTexture("Resources/piece_t.png");
+	pieceIndex[Piece_T].textureSize = { 60,40 };
 	pieceIndex[Piece_Z].offset = { {-1,-1,},{-1,0},{0,0},{0,1} };
 	pieceIndex[Piece_Z].blockData = { c,n,n,c };
 	pieceIndex[Piece_Z].previewTexture = LoadTexture("Resources/piece_z.png");
+	pieceIndex[Piece_Z].textureSize = { 60,40 };
 
 	//nextのいろいろ設定
 	for (size_t i = 0; i < nextSprites.size(); i++) {
@@ -50,7 +57,7 @@ void PieceManager::Initialize()
 		pos.x -= (size.x * 3) * i;
 
 		spritePos[i] = pos;
-
+		nextSprites[i]->SetSize(nextPiece[i].textureSize);
 		nextSprites[i]->SetPosition(pos);
 		nextSprites[i]->MatUpdate();
 
@@ -120,7 +127,12 @@ void PieceManager::DrawNextPiece()
 
 void PieceManager::UpdateNextQuete()
 {
+	for (size_t i = 0; i < nextSprites.size(); i++) {
+		//スプライトのサイズを持ってるテクスチャのサイズと合わせる
+		nextSprites[i]->SetSize(nextPiece[i].textureSize);
 
+		nextSprites[i]->MatUpdate();
+	}
 }
 
 void PieceManager::PieceSpawn()
@@ -146,6 +158,8 @@ void PieceManager::PieceSpawn()
 		nextPiece.erase(nextPiece.begin());
 		nextPiece.push_back(pieceIndex[randomIndex]);
 
+
+		UpdateNextQuete();
 	}
 
 }
